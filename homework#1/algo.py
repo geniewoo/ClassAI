0fa7dc7b9cd2843719570292d2e4cbdb74fb917d
SungWoo Park <SungWoo Park>
2017-03-21 05:04:40 +0900

    DFS done

--- homework#1/algo.py
+++ homework#1/algo.py
@@ -1,4 +1,43 @@
-class Dfs:
-    def firstFind(self, currentPuzzle) :
+import bisect
 
-        return 0, [123912340]
+direct = [-3, 1, 3, -1];
+answer = "123456780"
+def binarySearch(searchList, searchStr) :
+    first = 0
+    last = len(searchList) - 1
+    while 1 :
+        if first > last :
+            return 0, first
+        half = (last - first) // 2 + first
+        if searchList[half] == searchStr :
+            return 1, -1
+        elif searchList[half] < searchStr :
+            first = half + 1
+        else :
+            last = half - 1
+
+
+class Dfs:
+
+
+
+    def searchNext(self, currentPuzzle, openList, visitList, beforePosition) :
+        length = currentPuzzle[1];
+        if currentPuzzle[0] == answer :
+            return 1, [], [], [];
+
+        index = currentPuzzle[0].index('0')
+
+        for i in direct :
+            nextIndex =  i + index
+            if 0 <= nextIndex and nextIndex < 9 :
+                newNode = currentPuzzle[0]
+                newNode = newNode[: index] + newNode[nextIndex] + newNode[index + 1 :]
+                newNode = newNode[: nextIndex] + '0' + newNode[nextIndex + 1 :]
+
+                isExist, insertIndex = binarySearch(visitList, newNode);
+                if isExist == 0 :
+                    openList.append((newNode, length + 1))
+                    visitList.insert(insertIndex, newNode)
+
+        return 0, index, openList, visitList
--- homework#1/test.py
+++ homework#1/test.py
@@ -5,10 +5,31 @@ choose algorithmType
 2 : BFS
 3 : IDS
 """)
+openListSize = 0
 algorithmType = int(input())
-currentPuzzle = input()
+firstPuzzle = input()
 beforePosition = 0;
 if algorithmType == 1 :
     searchAlgorithm = algo.Dfs();
-beforePosition, visitMemory = searchAlgorithm.firstFind(currentPuzzle);
-print(beforePosition, visitMemory);
+success, beforePosition, openList, visitList = searchAlgorithm.searchNext((firstPuzzle, 0), [], [firstPuzzle], -1);
+if success :
+    print('fin')
+else :
+    print(success, beforePosition, openList, visitList)
+
+visit = 1
+while 1 :
+    visit += 1;
+    if len(openList) == 0 :
+        print('no answer')
+
+    currentPuzzle = openList.pop()
+    success, beforePosition, openList, visitList = searchAlgorithm.searchNext(currentPuzzle, openList, visitList, beforePosition)
+    if openListSize < len(openList) :
+        openListSize = len(openList)
+    if success :
+        print('fin', visit, openListSize)
+        break
+    if visit % 10000 == 0 :
+        print(visit)
+        print(openListSize, openList)
